7983d0cdb5e2592eb9d6ad8a0caea94be23c1e9c
yang <ywj9811@gmail.com>
2023-11-13 01:07:40 +0900

    feat : format종류 추가

--- src/main/java/christmas/view/constant/OutputFormat.java
+++ src/main/java/christmas/view/constant/OutputFormat.java
@@ -5,7 +5,20 @@ import java.text.DecimalFormat;
 public class OutputFormat {
     private final static DecimalFormat PRINT_FORMAT = new DecimalFormat("#,##0");
 
-    public static String formatter(int price) {
-        return PRINT_FORMAT.format(price);
+    public static String minusFormatter(int price) {
+        String format = PRINT_FORMAT.format(price);
+        return ("-" + format + "원\n");
+    }
+
+    public static String priceFormatter(int price) {
+        String format = PRINT_FORMAT.format(price);
+        return (format + "원");
+    }
+
+    public static String benefitFormatter(int price) {
+        String format = PRINT_FORMAT.format(price);
+        if (price > 0)
+            return ("-" + format + "원");
+        return (format + "원");
     }
 }
