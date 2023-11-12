d26824b1799079a3c41bf76e96c5d31666a9c9ae
yang <ywj9811@gmail.com>
2023-11-12 19:22:03 +0900

    feat : 혜택 계산 서비스 추가

---
+++ src/main/java/christmas/service/BenefitService.java
@@ -0,0 +1,66 @@
+package christmas.service;
+
+import christmas.domain.Menu;
+
+import java.util.List;
+
+import static christmas.domain.constant.Layer.DESERT;
+import static christmas.domain.constant.Layer.MAIN;
+
+public class BenefitService {
+    private final int date;
+    private final static List<Integer> WEEKDAY = List.of(1,4,5,6,7,8,11,12,13,14,15,18,19,20,21,22,25,26,27,28,29);
+    private final static List<Integer> WEEKEND = List.of(2,3,9,10,16,17,23,24,30,31);
+    private final static List<Integer> STAR_DAY = List.of(3,10,17,24,25,31);
+
+    public BenefitService(int date) {
+        this.date = date;
+    }
+
+
+    public int getD_dayBenefit() {
+        if (date > 25)
+            return 0;
+        return 1000+(100*(date-1));
+    }
+
+    public int getWeekDayBenefit(List<Menu> menus) {
+        if (WEEKDAY.contains(date)) {
+            return getDeserts(menus);
+        }
+        return 0;
+    }
+
+    private int getDeserts(List<Menu> menus) {
+        int desertBenefit = 0;
+        for (Menu menu : menus) {
+            if (menu.getLayer().equals(DESERT)) {
+                desertBenefit += 2023*menu.getAmount();
+            }
+        }
+        return desertBenefit;
+    }
+
+    public int getWeekEndBenefit(List<Menu> menus) {
+        if (WEEKEND.contains(date)) {
+            return getMain(menus);
+        }
+        return 0;
+    }
+
+    private int getMain(List<Menu> menus) {
+        int mainBenefit = 0;
+        for (Menu menu : menus) {
+            if (menu.getLayer().equals(MAIN)) {
+                mainBenefit += 2023*menu.getAmount();
+            }
+        }
+        return mainBenefit;
+    }
+
+    public int starDayBenefit() {
+        if (STAR_DAY.contains(date))
+            return 1000;
+        return 0;
+    }
+}
