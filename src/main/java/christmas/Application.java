d402176ca9269a78977dfde754b29fb07e36e127
yang <ywj9811@gmail.com>
2023-11-13 20:15:30 +0900

    feat : main 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,19 +1,17 @@
 package christmas;
 
 import christmas.controller.ChristmasEventController;
-import christmas.domain.Menu;
-
-import java.util.List;
+import christmas.domain.OrderMenus;
 
 public class Application {
     public static void main(String[] args) {
         ChristmasEventController controller = new ChristmasEventController();
         controller.greeting();
         int date = controller.greetingAndInputDate();
-        List<Menu> menus = controller.inputOrders(date);
+        OrderMenus menus = controller.inputOrders(date);
         controller.setServices(menus, date);
         int totalPriceBefore = controller.totalPriceBefore();
         controller.getBenefits(menus, totalPriceBefore);
-        controller.getResult(totalPriceBefore);
+        controller.getResult();
     }
 }
