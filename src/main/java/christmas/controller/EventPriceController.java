dd806484c031a4d1895710c6b70a022ea06a0ffe
yang <ywj9811@gmail.com>
2023-11-13 21:46:00 +0900

    refactor : controller 싱글톤 구현 및 분리

---
+++ src/main/java/christmas/controller/EventPriceController.java
@@ -0,0 +1,61 @@
+package christmas.controller;
+
+import christmas.domain.OrderMenus;
+import christmas.service.BenefitService;
+import christmas.service.PriceService;
+import christmas.view.OutputView;
+
+import static christmas.domain.constant.Benefit.*;
+import static christmas.domain.constant.Benefit.PRESENTATION;
+import static christmas.view.constant.OutputFormat.*;
+import static christmas.view.constant.OutputFormat.priceFormatter;
+
+public class EventPriceController {
+    private final OutputView outputView;
+    private final PriceService priceService;
+    private final BenefitService benefitService;
+    private final int[] benefits = new int[5];
+    private static EventPriceController instance;
+
+    private EventPriceController(OutputView outputView, PriceService priceService, BenefitService benefitService) {
+        this.outputView = outputView;
+        this.priceService = priceService;
+        this.benefitService = benefitService;
+    }
+
+    public static EventPriceController getInstance(OutputView outputView, PriceService priceService, BenefitService benefitService) {
+        if (instance == null)
+            return new EventPriceController(outputView, priceService, benefitService);
+        return instance;
+    }
+
+    public int totalPriceBefore() {
+        int totalPriceBefore = priceService.getTotalPriceBefore();
+        outputView.totalPriceBefore(priceFormatter(totalPriceBefore));
+        return totalPriceBefore;
+    }
+
+    public void getBenefits(OrderMenus menus, int totalPriceBefore) {
+        benefits[PRESENTATION.getColumn()] = benefitService.getPresentationBenefit(totalPriceBefore);
+        benefits[D_DAY.getColumn()] = benefitService.getD_dayBenefit(totalPriceBefore);
+        benefits[WEEKDAY.getColumn()] = benefitService.getWeekDayBenefit(menus, totalPriceBefore);
+        benefits[WEEKEND.getColumn()] = benefitService.getWeekEndBenefit(menus, totalPriceBefore);
+        benefits[STAR_DAY.getColumn()] = benefitService.getStarDayBenefit(totalPriceBefore);
+        String benefit = benefitHistoryFormatter(benefits);
+        outputView.presentationMenu(benefits[PRESENTATION.getColumn()]);
+        outputView.benefitHistory(benefit);
+    }
+
+    public void getResult() {
+        int totalBenefits = 0;
+        for (int benefitPrice : benefits)
+            totalBenefits += benefitPrice;
+
+        int totalPriceAfter = priceService.getTotalPriceAfter(totalBenefits, benefits[PRESENTATION.getColumn()]);
+        outputView.totalBenefit(benefitFormatter(totalBenefits));
+        outputView.totalPriceAfter(priceFormatter(totalPriceAfter));
+
+        String badge = benefitService.getBadge(totalBenefits);
+        outputView.badge(badge);
+    }
+}
