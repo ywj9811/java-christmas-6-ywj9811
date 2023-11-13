2d9352f18a2e3d838178ba224e8e3ff55687b5cc
yang <ywj9811@gmail.com>
2023-11-14 01:28:32 +0900

    fix : 싱글톤 반환 수정

--- src/main/java/christmas/service/PriceServiceImpl.java
+++ src/main/java/christmas/service/PriceServiceImpl.java
@@ -12,8 +12,15 @@ public class PriceServiceImpl implements PriceService{
 
     public static PriceService getInstance(OrderMenus menus) {
         if (instance == null)
-            return new PriceServiceImpl(menus);
+            instance = new PriceServiceImpl(menus);
         return instance;
+    }
+
+    /**
+     * 테스트용 싱글톤 해제 실전 사용X 절대 금지
+     */
+    public static PriceService reset(OrderMenus menus) {
+        return new PriceServiceImpl(menus);
     }
 
     @Override
